4941cb6411d9a6508a3a106613ae16f5fde413f4
mirandamutka <michirisu@outlook.com>
2019-05-18 13:34:07 +0200

    Update README and clear keys

--- fotoprat/constants/Config.js
+++ fotoprat/constants/Config.js
@@ -1,6 +1,6 @@
-export const FIREBASE_API_KEY = 'AIzaSyBjFEsO3tHzIO9qd1ZuBII7geWgSKG4bPk';
-export const FIREBASE_AUTH_DOMAIN = 'fotoprat-7256a.firebaseapp.com';
-export const FIREBASE_DATABASE_URL = 'https://fotoprat-7256a.firebaseio.com';
-export const FIREBASE_PROJECT_ID = 'fotoprat-7256a';
-export const FIREBASE_STORAGE_BUCKET = 'fotoprat-7256a.appspot.com';
-export const FIREBASE_MESSAGING_SENDER_ID = '730940702005';
+export const FIREBASE_API_KEY = 'YOUR API KEY';
+export const FIREBASE_AUTH_DOMAIN = 'YOUR AUTH DOMAIN';
+export const FIREBASE_DATABASE_URL = 'YOUR DATABASE URL';
+export const FIREBASE_PROJECT_ID = 'YOUR PROJECT ID';
+export const FIREBASE_STORAGE_BUCKET = 'YOUR STORAGE BUCKET';
+export const FIREBASE_MESSAGING_SENDER_ID = 'YOUR MESSAGING SENDER ID';
